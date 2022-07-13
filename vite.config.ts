1aef20a912c1ad4b0a5b51e8c72648d7cc128efa
kamalkech <maroc.develop@gmail.com>
2022-07-13 01:04:10 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
-// import mdx from "@mdx-js/rollup";
-import remarkGfm from "remark-gfm";
 import vercel from "solid-start-vercel";
 
 export default defineConfig({
   plugins: [
-    {
-      ...(await import("@mdx-js/rollup")).default({
-        jsx: true,
-        jsxImportSource: "solid-js",
-        providerImportSource: "solid-mdx",
-        remarkPlugins: [remarkGfm],
-      }),
-      enforce: "pre",
-    },
     solid({
-      extensions: [".mdx", ".md"],
       adapter: vercel(),
     }),
     // solid(),
